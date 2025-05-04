182d2c20ace6bab26902c7678fab3af96bd69c30
unknown <radityahendrianto@gmail.com>
2025-05-04 08:06:09 +0700

    cors

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 const todo = require('./src/routes/todoRoutes');
+const cors = require('cors');
 
+var corsOptions = {
+    origin: '*', // Ganti dengan URL frontend Anda
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204 // Untuk beberapa browser yang tidak mengharapkan status 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/home', todo);
